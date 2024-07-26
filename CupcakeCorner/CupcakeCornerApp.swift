ad2181e8fdf1703f05813c1036e7f4602e1dc735
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-26 17:24:01 -0400

    feat: Add TestUrlSessionView

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Rodrigo on 23-07-24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
