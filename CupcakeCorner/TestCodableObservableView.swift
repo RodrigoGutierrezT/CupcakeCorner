898a8423dc7cf3de056a95b1930c4d5802ca66ac
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-27 00:28:10 -0400

    feat: Add Codable Observable view

---
+++ CupcakeCorner/TestCodableObservableView.swift
@@ -0,0 +1,33 @@
+//
+//  TestCodableObservableView.swift
+//  CupcakeCorner
+//
+//  Created by Rodrigo on 27-07-24.
+//
+
+import SwiftUI
+
+@Observable
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+    
+    var name = "Taylor"
+}
+
+struct TestCodableObservableView: View {
+    var body: some View {
+        Button("Encode Taylor", action: encodeTaylor)
+    }
+    
+    func encodeTaylor() {
+        let data = try! JSONEncoder().encode(User())
+        let str = String(decoding: data, as: UTF8.self)
+        print(str)
+    }
+}
+
+#Preview {
+    TestCodableObservableView()
+}
